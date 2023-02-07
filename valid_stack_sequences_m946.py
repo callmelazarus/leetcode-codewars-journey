af8dcba02d2bd80c8f08587ac8ecf7ffd1cd3e7e
Jay <jayandrego@gmail.com>
2023-02-07 00:52:47 -0800

    valid stack sequence m946 attempt

--- valid_stack_sequences_m946.py
+++ valid_stack_sequences_m946.py
@@ -20,7 +20,50 @@ Given two integer arrays pushed and popped each with distinct values, return tru
 
 """
 # --- MY SOLUTION ---
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        """
+        Actually using a stack to solve this problem
+        pointer on push
 
+        while pu and po pointers are both not equal to the lenght of list
+        if pop doesn't equal push, push to stack, and advance pointer
+
+        if pop equals last value in stack, pop from stack, and advance pointer
+
+
+Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+                   ^                   ^
+stack = [1
+
+push(1), push(2), push(3), push(4),
+pop() -> 4,
+push(5),
+pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+
+when pointer reaches the end of the push and pop or the stack is empty, return true
+else return false
+
+        """
+        n = len(pushed)
+        pu = 1
+        po = 0
+        stack = []
+        stack.append(pushed[0])
+        while pu +1 <= n or po+1 <= n:
+            # push to stack
+            if stack[-1] == popped[po]:
+                stack.pop()
+                po += 1
+                print('pu: ', pu)
+                print('po: ', po)
+                print('stack: ', stack)
+            elif pu +1 == n and po+1 == n and stack == []:
+                return True
+            else:
+                stack.append(pushed[pu])
+                pu += 1
+        return False
 
 # --- TEST ---
 
